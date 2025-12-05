4b18a84e83ff4bdd7edcd9e65211b967581b8600
Lucas Luiz Guesser <lucas.lg17@aluno.ifsc.edu.br>
2025-12-05 19:05:27 -0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+function calcularConceitosFinais(dadosTurma){
+    let nomes = dadosTurma[0];
+    let pesos = dadosTurma[1];
+
+    let resultados = [];
+
+    for (let i = 0; i < nomes.length; i++) {
+        let notasAluno = [];
+
+        for (let lin = 2; lin < dadosTurma.length; lin++) {
+            notasAluno.push(dadosTurma[lin][i]);
+        }
+
+        let somaNotasPonderadas = 0;
+        let somaPesos = 0;
+
+        for (let j = 0; j < notasAluno.length; j++) {
+            somaNotasPonderadas += notasAluno[j] * pesos[j];
+            somaPesos += pesos[j];
+        }
+        
+        let conceitoFinal = somaNotasPonderadas / somaPesos;
+
+        resultados.push({
+            nome: nomes[i],
+            conceito: conceitoFinal.toFixed(2)
+        });
+    }
+    resultados.sort((a, b) => a.nome.localeCompare(b.nome));
+    return resultados;
+}
+
 let dadosTurma = [
     ["Lucas", "Ana", "Isabela"],
     [1, 2, 3],
@@ -6,50 +38,16 @@ let dadosTurma = [
     [7, 8, 6]
 ];
 
-let nomes = dadosTurma[0];
-let pesos = dadosTurma[1];
+let resultados = calcularConceitosFinais(dadosTurma);
 
-let resultados = [];
-
-for (let i = 0; i < nomes.length; i++) {
-    let notasAluno = dadosTurma[i + 2];
-
-    let somaNotasPonderadas = 0;
-    let somaPesos = 0;
-
-    for (let j = 0; j < notasAluno.length; j++) {
-        somaNotasPonderadas = somaNotasPonderadas + (notasAluno[j] * pesos[j]);
-        somaPesos = somaPesos + pesos[j];
-    }
-
-    let conceitoFinal = somaNotasPonderadas / somaPesos;
-
-    resultados.push({
-        nome: nomes[i],
-        conceito: conceitoFinal.toFixed(2)
-    });
-
-}
+let html = "<h2>Conceitos Finais da Turma</h2>";
 
 for (let i = 0; i < resultados.length; i++) {
-    for (let j = i + 1; j < resultados.length; j++) {
-        if (resultados[i].nome > resultados[j].nome) {
-            let temporario = resultados[i];
-            resultados[i] = resultados[j];
-            resultados[j] = temporario;
-        }
-    }
-    
+    html += "<li>" + resultados[i].nome + ": " + resultados[i].conceito + "</li>";
 }
 
-let html = "<h2>Conceitos Finais da Turma</h2><ul>"
-
-for (let i = 0; i < resultados.length; i++) {
-    html = html + "<li>" + resultados[i].nome + ": " + resultados[i].conceito + "</li>"; 
-}
-
-html = html + "</ul>";
+html += "</ul>";
 
 document.body.innerHTML = html;
 
-console.log("Resultados ordenados:", resultados);
+console.log("Resultados ordenados: ", resultados);
